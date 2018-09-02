95d890850d12b44b669c1e327bde21315cb9499e
VP Nagraj <vpnagraj@virginia.edu>
2018-09-02 17:40:41 -0400

    re-adding lessons and publish sh

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+clear
+
+cd public/
+rm -R *
+cd ../
+echo "--- Content cleared"
+hugo -v --ignoreCache    # try without cache
+echo "--- Hugo content generated"
+
+aws s3 sync --delete public/ s3://somrc-courses/
+echo "--- Public dir published to AWS"
+
+aws cloudfront create-invalidation --distribution-id "E9K683QE4HATG" --paths "/*"
+echo "--- CloudFront cache cleared"
